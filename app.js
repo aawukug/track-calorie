537d9089804df3d4fc680b9ddbd0122e6963ca40
Alexander Awuku <Alexawuku07@gmail.com>
2023-08-31 11:38:31 +0000

    adding calorie tracker project

---
+++ app.js
@@ -0,0 +1,249 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function() {
+	// Item Constructor
+	const Item = function(id, name, calories) {
+		this.id = id;
+		this.name = name;
+		this.calories = calories;
+	}
+
+	// Data Structure / State
+	const data = {
+		items: [
+		// { id: 0, name: 'Steak Dinner', calories: 1200},
+		// { id: 1, name: 'Cookie', calories: 400},
+		// { id: 2, name: 'Eggs', calories: 300},
+		],
+		currentItem: null, 
+		totalCalories: 0
+	}
+
+	// Public methods
+	return	{
+		getItems: function() {
+			return data.items
+		},
+
+		addItem: function(name, calories) {
+			let ID;
+			// Create ID
+			if (data.items.length > 0) {
+				ID = data.items[data.items.length -1].id + 1
+			} else {
+				ID = 0;
+			}
+
+			// Calories to number
+			calories = parseInt(calories);
+
+			// Create new item
+			newItem = new Item(ID, name, calories);
+
+			// Add to items array
+			data.items.push(newItem);
+
+
+			return newItem;
+		},
+		getTotalCalories: function() {
+			let total = 0;
+
+			// loop through items and add calories
+			data.items.forEach(item => {
+				total += item.calories;
+			});
+
+			// Set total calories in data structure
+			data.totalCalories = total;
+
+			// Return total
+			return data.totalCalories;
+		},
+
+		logData: function() {
+			return data;
+		}
+	}
+})();
+
+
+
+// UI Controller
+const UICtrl = (function() {
+	const UISelectors = {
+		itemList: '#item-list',
+		addBtn: '.add-btn',
+		updateBtn: '.update-btn',
+		deleteBtn: '.delete-btn',
+		backBtn: '.back-btn',
+		itemNameInput: '#item-name',
+		itemCaloriesInput: '#item-calories',
+		totalCalories: '.total-calories',
+
+	}
+
+	// Public methods
+	return {
+		populateItemList: function(items) {
+			let html = '';
+
+			items.forEach(item => {
+				html += `<li class="collection-item" id="item-${item.id}">
+ 				<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+ 				<a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+ 			</li>`;
+			});
+
+			// Insert list items
+			document.querySelector(UISelectors.itemList).innerHTML = html;
+		},
+
+		getItemInput: function() {
+			return {
+				name:document.querySelector(UISelectors.itemNameInput).value,
+				calories:document.querySelector(UISelectors.itemCaloriesInput).value
+			}
+		}, 
+
+		addListItem: function(item) {
+			// Show the list
+			document.querySelector(UISelectors.itemList).style.display = 'block';
+			// Create li element
+			const li = document.createElement('li'); 
+			// Add class
+			li.className = 'collection-item';
+			// Add ID
+			li.id = `item-${item.id}`;
+			// Add HTML
+			li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+ 				<a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>`;
+ 			// Insert item
+ 			document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+		},
+		hideList: function() {
+			document.querySelector(UISelectors.itemList).style.display = 'none'
+		},
+		clearInput: function() {
+			document.querySelector(UISelectors.itemNameInput).value = '';
+			document.querySelector(UISelectors.itemCaloriesInput).value = '';
+		},
+		showTotalCalories: function(totalCalories) {
+			document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+		},
+		clearEditState: function(){
+			UICtrl.clearInput();
+			document.querySelector(UISelectors.updateBtn).style.display = 'none';
+			document.querySelector(UISelectors.deleteBtn).style.display = 'none';
+			document.querySelector(UISelectors.backBtn).style.display = 'none';
+			document.querySelector(UISelectors.addBtn).style.display = 'inline';
+		},
+
+		getSelectors: function() {
+			return UISelectors;
+		}
+
+	}
+})();
+
+
+
+// App Controller
+const App = (function(ItemCtrl, UiCtrl) {
+	//Load event listeners
+	const loadEventlisteners = function() {
+		// Get UI Selectors
+		const UISelectors = UICtrl.getSelectors();
+
+		// Add item event
+		['click','keydown'].forEach(e => 
+   		 document.querySelector(UISelectors.addBtn).addEventListener(e, itemAddSubmit, false)
+		);
+		// document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+
+		// Edit icon click event
+		document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+
+	}
+
+	// Add item submit
+	const itemAddSubmit = function(e) {
+		// Get form input from UI Controller
+		const input = UICtrl.getItemInput();
+
+		// Check for name, calorie input and keypress event
+		if (input.name !== '' && input.calories !== '' || keydown === 13) {
+			// Add item
+
+			const newItem = ItemCtrl.addItem(input.name, input.calories);
+
+			// Add item to UI list
+			UICtrl.addListItem(newItem);
+
+			// Get total calories
+			const totalCalories = ItemCtrl.getTotalCalories();
+
+			// Add total calories to the UI
+			UICtrl.showTotalCalories(totalCalories);
+
+			// Clear fields
+			UICtrl.clearInput();
+
+		}
+
+		e.preventDefault();
+	}
+
+	// Update item submit
+	const itemUpdateSubmit = function(e) {
+		if (e.target.classList.contains('edit-item')) {
+			// Get list item id(item-0, item-1)
+			const listId = e.target.parentNode.parentNode.id;
+			
+			// Break into in array
+			const listIdArr = listId.split('-');
+			
+			// Get the actual id
+			const id = parseInt(listIdArr[1]);
+		}
+
+		e.preventDefault();
+	}
+
+
+	// Public methods
+	return	{
+		init: function() {
+			// Clear edit state/set initial state
+			UICtrl.clearEditState();
+			
+
+			// Fetch items from data Structure
+			const items = ItemCtrl.getItems();
+
+			// Check if any items 
+			if (items.length === 0) {
+				UICtrl.hideList();
+			} else {
+				// Populate list with items
+				UICtrl.populateItemList(items);
+			}
+
+			// Get total calories
+			const totalCalories = ItemCtrl.getTotalCalories();
+
+			// Add total calories to the UI
+			UICtrl.showTotalCalories(totalCalories);
+
+			// Load event listeners
+			loadEventlisteners();
+
+		}
+	}
+
+})(ItemCtrl, UICtrl);
+
+// initialize App
+App.init();
+
