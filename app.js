7e67df51de7871f49348a080ccaac8a72e3b1535
Alexander Awuku <Alexawuku07@gmail.com>
2023-09-05 15:25:47 +0000

    working on update, delete and clear all button from local storage

--- app.js
+++ app.js
@@ -31,6 +31,31 @@ const StorageCtrl = (function() {
 				items = JSON.parse(localStorage.getItem('items'))
 			}
 			return items;
+		},
+		updatedItemStorage: function(updatedItem) {
+			let items = JSON.parse(localStorage.getItems('items'));
+
+			items.forEach(function(item, index){
+				if (updatedItem.id === item.id) {
+					items.splice(index, 1, updatedItem);
+				}
+			});
+			localStorage.setItem('items', JSON.stringify(items));
+
+		},
+		deleteItemFromStorage: function(id) {
+			let items = JSON.parse(localStorage.getItem('items'));
+
+			items.forEach(function(item, index){
+				if (id === item.id) {
+					items.splice(index, 1);
+				}
+			});
+			localStorage.setItem('items', JSON.stringify(items));
+
+		},
+		clearItemsFromStorage: function(){
+			localStorage.removeItem('items');
 		}
 	}
 })();
@@ -387,6 +412,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 		// Add total calories to the UI
 		UICtrl.showTotalCalories(totalCalories);
 
+		// Update local storage
+		StorageCtrl.updatedItemStorage(updatedItem)
+
 		UICtrl.clearEditState();
 
 		e.preventDefault();
@@ -409,6 +437,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 		// Add total calories to the UI
 		UICtrl.showTotalCalories(totalCalories);
 
+		// Delete from local storage
+		StorageCtrl.deleteItemFromStorage(currentItem.id);
+
 		UICtrl.clearEditState();
 
 		e.preventDefault();
@@ -427,6 +458,9 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 
 		// Remove from UI
 		UICtrl.removeItems();
+
+		// Clear from local storage
+		StorageCtrl.clearItemsFromStorage();
 
 		// Hide UL
 		UICtrl.hideList();
